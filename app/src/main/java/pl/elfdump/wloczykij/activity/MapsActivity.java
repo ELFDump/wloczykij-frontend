3498236547ad919c7b955af95ceafaae0dd5a03a
Adikso <adam@zambrzycki.net>
2016-10-15 23:54:49 +0200

    Added Google API login\nAdded Map

---
+++ app/src/main/java/pl/elfdump/wloczykij/activity/MapsActivity.java
@@ -0,0 +1,39 @@
+package pl.elfdump.wloczykij.activity;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import pl.elfdump.wloczykij.R;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng lodz = new LatLng(51.759248, 19.455983);
+        mMap.addMarker(new MarkerOptions().position(lodz).title("Łódź").snippet("Opis"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lodz, 15));
+    }
+}
