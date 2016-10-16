2b8711b9283bb5cc7061deff6a00d6a72bab89c3
Adikso <adam@zambrzycki.net>
2016-10-16 18:25:05 +0200

    Added getters to User

--- app/src/main/java/pl/elfdump/wloczykij/models/User.java
+++ app/src/main/java/pl/elfdump/wloczykij/models/User.java
@@ -5,4 +5,20 @@ public class User {
     private String username;
     private String first_name;
     private String last_name;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
 }
