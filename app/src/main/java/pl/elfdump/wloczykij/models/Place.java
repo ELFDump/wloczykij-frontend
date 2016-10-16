6a4c007c64ede3d756600e2040ec9f5012d41be6
Damian Płóciennik <damian.plociennik@mybboard.pl>
2016-10-16 22:56:57 +0200

    Adde Place class

---
+++ app/src/main/java/pl/elfdump/wloczykij/models/Place.java
@@ -0,0 +1,12 @@
+package pl.elfdump.wloczykij.models;
+
+public class Place {
+    private String url;
+    private String name;
+    private String author;
+    private String date_created;
+    private String date_modified;
+    private Float[] coords;
+    private String[] photos;
+    private String[] tags;
+}
