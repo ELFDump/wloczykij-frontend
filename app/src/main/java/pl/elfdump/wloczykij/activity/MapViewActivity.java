5af34d33e814bb5c2482d9bb3dc3e28ac397355e
krzys-h <krzys_h@interia.pl>
2016-10-18 21:25:58 +0200

    Show my location on map

--- app/src/main/java/pl/elfdump/wloczykij/activity/MapViewActivity.java
+++ app/src/main/java/pl/elfdump/wloczykij/activity/MapViewActivity.java
@@ -1,9 +1,14 @@
 package pl.elfdump.wloczykij.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,6 +16,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import pl.elfdump.wloczykij.R;
 import pl.elfdump.wloczykij.Wloczykij;
 import pl.elfdump.wloczykij.data.DataManager;
@@ -18,11 +24,14 @@ import pl.elfdump.wloczykij.data.map.MarkerManager;
 import pl.elfdump.wloczykij.utils.MapUtilities;
 
 import java.util.List;
+
 import pl.elfdump.wloczykij.network.api.models.Place;
 
 public class MapViewActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+
+    private static final int MAP_LOCATION_PERMISSION_REQUEST = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +39,7 @@ public class MapViewActivity extends AppCompatActivity implements OnMapReadyCall
         setContentView(R.layout.activity_map_view);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+            .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
     }
@@ -39,9 +48,15 @@ public class MapViewActivity extends AppCompatActivity implements OnMapReadyCall
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setMapToolbarEnabled(false);
-
-        LatLng lodz = new LatLng(51.759248, 19.455983);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lodz, 15));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            }, MAP_LOCATION_PERMISSION_REQUEST);
+        } else {
+            mMap.setMyLocationEnabled(true);
+        }
 
         DataManager dataManager = Wloczykij.getSession().dataManager;
         List<Place> places = dataManager.placeStorage.getPlaces();
@@ -56,6 +71,17 @@ public class MapViewActivity extends AppCompatActivity implements OnMapReadyCall
         }
 
         mMap.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MAP_LOCATION_PERMISSION_REQUEST) {
+            if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+            }
+        }
     }
 
     @Override
