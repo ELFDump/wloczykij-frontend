0a665816fc106816ff6aef7ecca957d1bc5ee2bf
krzys-h <krzys_h@interia.pl>
2016-10-23 21:41:53 +0200

    Fix tag list problems

--- app/src/main/java/pl/elfdump/wloczykij/ui/PlaceDetailsListAdapter.java
+++ app/src/main/java/pl/elfdump/wloczykij/ui/PlaceDetailsListAdapter.java
@@ -2,6 +2,7 @@ package pl.elfdump.wloczykij.ui;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,66 +19,44 @@ public class PlaceDetailsListAdapter extends ArrayAdapter<PlaceDetailsItem> {
 
     private final Context context;
     private final ArrayList<PlaceDetailsItem> modelsArrayList;
-
-    // Stores checkboxes state
-    private boolean[] isChecked;
-
-    /**
-        We are using ViewHolder store layouts to reuse them
-        on ListView updates which are happening during scrolling long lists
-        Increases performance a lot
-     **/
-    private static class ViewHolder {
-        private TextView titleView;
-        private CheckBox checkBox;
-    }
+    private boolean[] checked;
 
     public PlaceDetailsListAdapter(Context context, ArrayList<PlaceDetailsItem> modelsArrayList) {
         super(context, R.layout.place_details_item, modelsArrayList);
 
         this.context = context;
         this.modelsArrayList = modelsArrayList;
-        this.isChecked = new boolean[getCount()];
+        this.checked = new boolean[modelsArrayList.size()];
     }
 
+    @NonNull
     @Override
-    public View getView(final int position, View convertView, final ViewGroup parent) {
+    public View getView(final int position, View convertView, @NonNull ViewGroup parent) {
         View rowView = convertView;
-        final int realPosition = ((ListView) parent).getFirstVisiblePosition() + position;
-
-        if(rowView == null){
+        if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
             rowView = inflater.inflate(R.layout.place_details_item, parent, false);
-
-            ViewHolder viewHolder = new ViewHolder();
-            viewHolder.titleView = (TextView) rowView.findViewById(R.id.item_title);
-            viewHolder.checkBox = (CheckBox) rowView.findViewById(R.id.item_checkBox);
-
-            // Checked status is changing on list scrolling
-            viewHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean checked) {
-                    isChecked[realPosition] = checked; // .. so after user select checkbox we store state
-                }
-            });
-
-            viewHolder.checkBox.setChecked(isChecked[position]);
-            rowView.setTag(viewHolder);
         }
 
-        ViewHolder holder = (ViewHolder) rowView.getTag();
-        TextView titleView = holder.titleView;
-        CheckBox checkBox = holder.checkBox;
-        checkBox.setChecked(isChecked[realPosition]);
+        TextView titleView = (TextView) rowView.findViewById(R.id.item_title);
+        CheckBox checkBox = (CheckBox) rowView.findViewById(R.id.item_checkBox);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean checked) {
+                PlaceDetailsListAdapter.this.checked[position] = checked;
+            }
+        });
 
         Drawable img = ContextCompat.getDrawable(context, modelsArrayList.get(position).getIcon());
         img.setBounds( 0, 0, 100, 100 );
         titleView.setCompoundDrawables(img, null, null, null);
 
-        titleView.setText(modelsArrayList.get(position).getTitle());
+        titleView.setText(modelsArrayList.get(position).getTitle() + " [" + String.valueOf(position) + "]"); // TODO: ID for debugging only
         checkBox.setVisibility((modelsArrayList.get(position).hasCheckBox() ? View.VISIBLE : View.GONE));
+        checkBox.setChecked(this.checked[position]);
 
         if(modelsArrayList.get(position).getType() == PlaceDetailsItem.ITEM_CHILD){ //
             rowView.setPadding(100, 0, 0, 0);
