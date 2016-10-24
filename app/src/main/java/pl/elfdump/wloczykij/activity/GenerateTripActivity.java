3a1ca3b34bbbd6012d4adc5329d26495a06cfa49
Adikso <adam@zambrzycki.net>
2016-10-24 21:18:31 +0200

    Updated GenerateTripActivity layout

--- app/src/main/java/pl/elfdump/wloczykij/activity/GenerateTripActivity.java
+++ app/src/main/java/pl/elfdump/wloczykij/activity/GenerateTripActivity.java
@@ -1,10 +1,14 @@
 package pl.elfdump.wloczykij.activity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 import com.klinker.android.sliding.SlidingActivity;
 import pl.elfdump.wloczykij.R;
+import pl.elfdump.wloczykij.ui.TagsSelectorController;
 
 public class GenerateTripActivity extends SlidingActivity {
+
+    private TagsSelectorController tagsSelectorController;
 
     @Override
     public void init(Bundle savedInstanceState) {
@@ -12,6 +16,11 @@ public class GenerateTripActivity extends SlidingActivity {
         enableFullscreen();
 
         setContent(R.layout.activity_generate_trip);
+
+        ListView listView = (ListView) findViewById(R.id.tag_selector_buttons_row);
+
+        tagsSelectorController = new TagsSelectorController(listView, null);
+        tagsSelectorController.loadData();
     }
 }
 
